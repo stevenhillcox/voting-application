d14648bb895d02b86547f8bb96d22bec8baed4e0
Steven Hillcox <shillcox@scottlogic.co.uk>
2015-06-15 14:52:53 +0100

    Remove timeouts

--- VotingApplication/VotingApplication.Web.Tests/E2E/E2ETest.cs
+++ VotingApplication/VotingApplication.Web.Tests/E2E/E2ETest.cs
@@ -51,8 +51,8 @@ namespace VotingApplication.Web.Tests.E2E
             get
             {
                 var driver = new NgWebDriver(new ChromeDriver(ChromeDriverDir));
-                driver.Manage().Timeouts().SetScriptTimeout(TimeSpan.FromSeconds(int.MaxValue));
-                driver.Manage().Timeouts().SetPageLoadTimeout(TimeSpan.FromSeconds(int.MaxValue));
+                //driver.Manage().Timeouts().SetScriptTimeout(TimeSpan.FromSeconds(int.MaxValue));
+                //driver.Manage().Timeouts().SetPageLoadTimeout(TimeSpan.FromSeconds(int.MaxValue));
 
                 return driver;
             }
